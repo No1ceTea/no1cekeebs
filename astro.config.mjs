f8da678a7fd8b91291687e23915488b0f4b443f6
No1ceTea <thaiwomtg@gmail.com>
2023-02-16 16:22:16 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind' 
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://example.com',
+  integrations: [
+    sitemap(),
+    react(),
+    image(),
+    tailwind()
+  ],
+})
