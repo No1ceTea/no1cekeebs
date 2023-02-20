20a1d6cdebc771a580cf1f290a6ac8658dce8fa3
No1ceTea <thaiwomtg@gmail.com>
2023-02-20 19:34:32 +0100

    dev >

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://no1cetea-keyboard.vercel.app/',
   integrations: [
     sitemap(),
     react(),
