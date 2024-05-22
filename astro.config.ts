aa78e2d84f10a74b1a2ddd08906edc10543bffa7
Jacquot Thai <71291683+No1ceTea@users.noreply.github.com>
2024-05-22 12:15:38 +0000

    Update npm dependencies for Astro and Vercel adapters

--- astro.config.ts
+++ astro.config.ts
@@ -5,17 +5,10 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
