39060a9735e3c8af5407449187a46b8bb3b2d097
Jacquot Thai <71291683+No1ceTea@users.noreply.github.com>
2024-10-08 13:24:32 +0000

    fix: fixing node vercel

--- api/submit-review.js
+++ api/submit-review.js
@@ -1,29 +1,19 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    try {
-      const { name, email, keyboardModel, experience, reason } = req.body;
+    // Récupérez les données du corps de la requête
+    const { name, email, keyboardModel, experience, reason } = req.body;
 
-      // Ici, vous pouvez ajouter une logique de validation des données
-      if (!name || !email || !keyboardModel || !experience || !reason) {
-        return res.status(400).json({ message: 'Tous les champs sont requis.' });
-      }
+    // Ici, vous pouvez traiter les données comme vous le souhaitez
+    // Par exemple, les enregistrer dans une base de données, envoyer un email, etc.
 
-      // Ici, vous pouvez ajouter la logique pour sauvegarder les données
-      // Par exemple, les enregistrer dans une base de données, envoyer un email, etc.
-      console.log('Nouvelle candidature reçue:', { name, email, keyboardModel, experience, reason });
+    // Pour cet exemple, nous allons simplement les logger
+    console.log('Nouvelle candidature reçue:', { name, email, keyboardModel, experience, reason });
 
-      // Simulons un délai pour montrer le comportement de chargement
-      await new Promise(resolve => setTimeout(resolve, 1000));
-
-      // Envoyez une réponse de succès
-      res.status(200).json({ message: 'Votre candidature a été soumise avec succès !' });
-    } catch (error) {
-      console.error('Erreur lors du traitement de la candidature:', error);
-      res.status(500).json({ message: 'Une erreur est survenue lors du traitement de votre candidature.' });
-    }
+    // Envoyez une réponse de succès
+    res.status(200).json({ message: 'Candidature reçue avec succès' });
   } else {
     // Méthode non autorisée si ce n'est pas une requête POST
     res.setHeader('Allow', ['POST']);
-    res.status(405).json({ message: `Méthode ${req.method} non autorisée` });
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
--- astro.config.ts
+++ astro.config.ts
@@ -5,13 +5,13 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import vercel from '@astrojs/vercel/serverless';
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  output: "server",
-  adapter: vercel({
+  output: "static",
+  adapter: vercelStatic({
     imageService: true,
     webAnalytics: {
       enabled: true,
